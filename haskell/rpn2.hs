29bc2a52a182cbb7c2a2189883e02600545c869e
Filipe Pires Guimaraes <filipepires@MacBook-Pro-Retina-de-Filipe.local>
2017-11-22 13:31:33 -0300

    Fix: alteracao tab para space

--- haskell/rpn2.hs
+++ haskell/rpn2.hs
@@ -4,51 +4,52 @@ import Data.Char (toLower)
 type Stack = [Double]
 
 opBin :: String -> Stack -> Stack
-opBin _ [] = []			--talvez lançar um erro, se for na GUI ver como dar um aviso
-opBin _ (x:[]) = x:[]	--nao opera sobre apenas um operando
+opBin _ [] = [] --talvez lanï¿½ar um erro, se for na GUI ver como dar um aviso
+opBin _ (x:[]) = x:[] --nao opera sobre apenas um operando
 opBin c (x:y:xs)
-	| c == "+" = (y+x):xs
-	| c == "-" = (y-x):xs
-	| c == "*" = (y*x):xs
-	| c == "/" = (y/x):xs
-	
-opUn ::	String -> Stack ->Stack
+ | c == "+" = (y+x):xs
+ | c == "-" = (y-x):xs
+ | c == "*" = (y*x):xs
+ | c == "/" = (y/x):xs
+ | c == "^" = (y**x):xs
+
+opUn :: String -> Stack -> Stack
 opUn _ [] = []
 opUn c (x:xs)
-	| c == "ln" = (log x):xs
-	| c == "exp" = (exp x):xs
-	| c == "sqrt" = (sqrt x):xs
-	| c == "sin" = (sin x):xs
-	| c == "cos" = (cos x):xs
-	| c == "tan" = (tan x):xs
+ | c == "ln" = (log x):xs
+ | c == "exp" = (exp x):xs
+ | c == "sqrt" = (sqrt x):xs
+ | c == "sin" = (sin x):xs
+ | c == "cos" = (cos x):xs
+ | c == "tan" = (tan x):xs
 
 isNumber :: String -> Bool
 isNumber str =
-	case (reads str) :: [(Double, String)] of
-		[(_,"")] -> True
-		_		 -> False	
+ case (reads str) :: [(Double, String)] of
+  [(_,"")] -> True
+  _   -> False
 
 
-{- opção para caso use uma caixa de entrada a parte para ir entrando com cada comando
-recebe o comando como uma string, para decidir que operação realizar e a pilha, retornando
+{- opï¿½ï¿½o para caso use uma caixa de entrada a parte para ir entrando com cada comando
+recebe o comando como uma string, para decidir que operaï¿½ï¿½o realizar e a pilha, retornando
 a pilha devidamente modificada -}
 oper :: String -> Stack -> Stack
 oper c x
-	| isNumber c = ((read c::Double):x)
-	| c `elem` ["+","-","*","/"] = opBin c x
-	| c `elem` ["ln", "exp", "sqrt", "sin", "cos", "tan"] = opUn c x
-	
+ | isNumber c = ((read c::Double):x)
+ | c `elem` ["+","-","*","/"] = opBin c x
+ | c `elem` ["ln", "exp", "sqrt", "sin", "cos", "tan"] = opUn c x
+
 calc :: Stack -> IO()
 calc xs = do
-	system "cls" >> print xs
-	comando <- getLine
-	let comm = (map toLower comando) in
-		if comm == "quit"
-			then return ()
-			else calc (oper comm xs) -- calc retorna uma stack modificada, então dá pra chamar calc de novo em cima da nova stack
-	
+ system "clear" >> print xs
+ --system "cls" >> print xs
+ comando <- getLine
+ let comm = (map toLower comando) in
+  if comm == "quit"
+   then return ()
+   else calc (oper comm xs) -- calc retorna uma stack modificada, entï¿½o dï¿½ pra chamar calc de novo em cima da nova stack
+
 main :: IO()
 main = do
-	calc []
-	return ()
-	
+ calc []
+ return ()
