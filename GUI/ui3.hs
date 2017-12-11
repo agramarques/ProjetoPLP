f1e38642b9f115477d58eb84112eb002e5b82e46
agramarques <agramarques@gmail.com>
2017-12-11 09:25:50 -0300

    continuando interface em haskell

--- GUI/ui3.hs
+++ GUI/ui3.hs
@@ -24,6 +24,20 @@ main = do
   bASin <- buttonNewWithLabel "asin"
   bACos <- buttonNewWithLabel "acos"
   bATan <- buttonNewWithLabel "atan"
+  bSqrt <- buttonNewWithLabel "sqrt"
+  bP2 <- buttonNewWithLabel "x²"
+  bFat <- buttonNewWithLabel "x!"
+  bPow <- buttonNewWithLabel "y^x"
+  bRoot <- buttonNewWithLabel "y^(1/x)"
+  bInv <- buttonNewWithLabel "1/x"
+  bLn <- buttonNewWithLabel "ln"
+  bLog10 <- buttonNewWithLabel "log10"
+  bLog2 <- buttonNewWithLabel "log2"
+  bExp <- buttonNewWithLabel "exp"
+  bE10 <- buttonNewWithLabel "10^x"
+  bE2 <- buttonNewWithLabel "2^x"
+  bComb <- buttonNewWithLabel "yCx"
+  bArr <- buttonNewWithLabel "yAx"
   table3 <- tableNew 4 2 True
   bPol <- buttonNewWithLabel "Coord Polares"
   bCart <- buttonNewWithLabel "Coord Cartes"
@@ -57,25 +71,42 @@ main = do
   boxPackStart box1 box2 PackGrow 10
   boxPackStart box1 box3 PackGrow 10
   boxPackStart box2 box4 PackGrow 10
-  boxPackStart box2 table2 PackGrow 10
   boxPackStart box4 stack PackGrow 10
   boxPackStart box4 labelErros PackGrow 10
   boxPackStart box4 box5 PackGrow 10
   boxPackStart box5 bSwap PackGrow 5
   boxPackStart box5 bClear PackGrow 5
+  
+  boxPackStart box2 table2 PackGrow 10
   tableAttachDefaults table2 bSin 0 1 0 1
   tableAttachDefaults table2 bCos 1 2 0 1
   tableAttachDefaults table2 bTan 2 3 0 1
   tableAttachDefaults table2 bASin 0 1 1 2
   tableAttachDefaults table2 bACos 1 2 1 2
   tableAttachDefaults table2 bATan 2 3 1 2
+  tableAttachDefaults table2 bSqrt 0 1 2 3
+  tableAttachDefaults table2 bP2 1 2 2 3
+  tableAttachDefaults table2 bFat 2 3 2 3
+  tableAttachDefaults table2 bPow 0 1 3 4
+  tableAttachDefaults table2 bRoot 1 2 3 4
+  tableAttachDefaults table2 bInv 2 3 3 4
+  tableAttachDefaults table2 bLn 0 1 4 5
+  tableAttachDefaults table2 bLog10 1 2 4 5
+  tableAttachDefaults table2 bLog2 2 3 4 5
+  tableAttachDefaults table2 bExp 0 1 5 6
+  tableAttachDefaults table2 bE10 1 2 5 6
+  tableAttachDefaults table2 bE2 2 3 5 6
+  tableAttachDefaults table2 bComb 0 1 6 7
+  tableAttachDefaults table2 bArr 1 2 6 7
   tableSetRowSpacings table2 5
   tableSetColSpacings table2 5
+
   boxPackStart box3 table3 PackGrow 10
   tableAttachDefaults table3 bPol 0 1 0 1
   tableAttachDefaults table3 bCart 1 2 0 1
   tableSetRowSpacings table3 5
   tableSetColSpacings table3 5
+
   boxPackStart box3 table1 PackGrow 10
   tableAttachDefaults table1 b1 0 1 0 1
   tableAttachDefaults table1 b2 1 2 0 1
@@ -93,7 +124,7 @@ main = do
   tableAttachDefaults table1 b0 1 2 3 4
   tableAttachDefaults table1 bDot 2 3 3 4
   tableAttachDefaults table1 bDiv 3 4 3 4
-  
+
   boxPackStart box3 table4 PackGrow 10
   tableAttachDefaults table4 bSum 0 1 0 1
   tableAttachDefaults table4 lStat 1 2 0 1
