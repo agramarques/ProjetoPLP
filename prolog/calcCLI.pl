ad842c61a5f08931b6831593c0e1d6857da652ce
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-02-15 18:46:46 -0300

    operador -> não é implicacao de fato, rip prolog

--- prolog/calcCLI.pl
+++ prolog/calcCLI.pl
@@ -4,14 +4,13 @@
 
 help() :-
     write('help'),
-    nl,
-    main().
+    nl.
 
 main :-
     read_line_to_codes(user_input, Input),
     string_to_atom(Input, I),
 
-    ( (I = 'help'; I = 'ajuda') -> help()
-    ; (I = 'exit'; I = 'quit'; I = 'sair') -> halt(0)
-    ; main()
-    ).
+    (\+(I = 'help'; I = 'ajuda') ; help()),
+    (\+(I = 'exit'; I = 'quit'; I = 'sair') ; halt(0)),
+
+    main().
