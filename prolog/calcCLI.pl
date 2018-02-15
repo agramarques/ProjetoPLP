cd130ca2f9627e869e3e8bdaa7126aac402472d6
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-02-15 18:00:43 -0300

    esqueleto para a interacao com o usuario da calc versao cli

---
+++ prolog/calcCLI.pl
@@ -0,0 +1,17 @@
+:- [calc].
+:- initialization main.
+
+
+help() :-
+    write('help'),
+    nl,
+    main().
+
+main :-
+    read_line_to_codes(user_input, Input),
+    string_to_atom(Input, I),
+
+    ( (I = 'help'; I = 'ajuda') -> help()
+    ; (I = 'exit'; I = 'quit'; I = 'sair') -> halt(0)
+    ; main()
+    ).
