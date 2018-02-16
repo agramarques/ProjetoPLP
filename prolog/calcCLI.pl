b97244741948e648f4b7d452265085707b140983
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-02-16 03:57:52 -0300

    ja da pra fazer um somatorio pelo menos

--- prolog/calcCLI.pl
+++ prolog/calcCLI.pl
@@ -7,10 +7,15 @@ help() :-
     nl.
 
 main :-
+    main([]).
+
+main(Stack) :-
     read_line_to_codes(user_input, Input),
     string_to_atom(Input, I),
 
-    (\+(I = 'help'; I = 'ajuda') ; help()),
+    (\+(I = 'help'; I = 'ajuda') ; help(), main(Stack)),
     (\+(I = 'exit'; I = 'quit'; I = 'sair') ; halt(0)),
 
-    main().
+    (  atom_number(I, N) 
+    -> main([N|Stack])
+    ;  oper(I, Stack, [R|Rs]), write(R), nl, main([R|Rs])).
